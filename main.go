b19b9bef254105a64b51da43066408e0f913346e
idriss30 <idriss.cissoko@hotmail.com>
2022-10-02 09:35:04 -0400

    main function created

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+func main() {
+	const conferenceName = "Go conference"
+	const numberOfTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v\n", conferenceName)
+	fmt.Printf("We have %v tickets and still have %v left \n", numberOfTickets, remainingTickets)
+	fmt.Println("Get your tickets here now!")
+
+	var firstName, lastName, email, ticketsBought string
+
+	fmt.Println("please enter your first name")
+	fmt.Scan(&firstName)
+	fmt.Println("please enter your last name")
+	fmt.Scan(&lastName)
+	fmt.Println("please enter your email")
+	fmt.Scan(&email)
+	fmt.Println("how many tickets do you need ?")
+	fmt.Scan(&ticketsBought)
+
+	fmt.Printf("Thank you mister %v %v for purchasing %v ticket(s).\n you will receive a confirmation at %v\n", firstName, lastName, ticketsBought, email)
+
+}
