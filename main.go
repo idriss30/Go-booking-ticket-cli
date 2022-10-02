b342af112136eb30081ad96851e51259ef6a2aad
idriss30 <idriss.cissoko@hotmail.com>
2022-10-02 19:15:42 -0400

    added specific type to variable and created condition to run the program until all ticket are booked

--- main.go
+++ main.go
@@ -1,27 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	const conferenceName = "Go conference"
 	const numberOfTickets = 50
-	var remainingTickets = 50
+	 remainingTickets := uint(50)
+	 usersRegistered :=[]string{}
+	for {
+		fmt.Printf("Welcome to %v\n", conferenceName)
+		fmt.Printf("Initial ticket count : %v\n", numberOfTickets)
 
-	fmt.Printf("Welcome to %v\n", conferenceName)
-	fmt.Printf("We have %v tickets and still have %v left \n", numberOfTickets, remainingTickets)
-	fmt.Println("Get your tickets here now!")
+		var firstName, lastName, email string
+		var ticketsBought uint
 
-	var firstName, lastName, email, ticketsBought string
+		fmt.Println("please enter your first name")
+		fmt.Scan(&firstName)
+		fmt.Println("please enter your last name")
+		fmt.Scan(&lastName)
+		fmt.Println("please enter your email")
+		fmt.Scan(&email)
+		fmt.Println("how many tickets do you need ?")
+		fmt.Scan(&ticketsBought)
+		if ticketsBought > remainingTickets{
+			fmt.Printf("we don't have %v tickets left", ticketsBought )
+			break
+		}
+		remainingTickets = remainingTickets - ticketsBought
+		usersRegistered = append(usersRegistered, firstName+" "+lastName)
 
-	fmt.Println("please enter your first name")
-	fmt.Scan(&firstName)
-	fmt.Println("please enter your last name")
-	fmt.Scan(&lastName)
-	fmt.Println("please enter your email")
-	fmt.Scan(&email)
-	fmt.Println("how many tickets do you need ?")
-	fmt.Scan(&ticketsBought)
+		fmt.Printf("Thank you mister %v %v for purchasing %v ticket(s).\nyou will receive a confirmation at %v\n", firstName, lastName, ticketsBought, email)
+		fmt.Printf("the remaining ticket count is %v\n", remainingTickets)
+		
+		firstNames := []string{}
+		for _, name := range usersRegistered{
+			fullName := strings.Fields(name)
+			firstNames = append(firstNames, fullName[0])
+		   
+		}
 
-	fmt.Printf("Thank you mister %v %v for purchasing %v ticket(s).\n you will receive a confirmation at %v\n", firstName, lastName, ticketsBought, email)
+		fmt.Printf("the following people will be present %v\n", firstNames)
+
+	}
 
 }
